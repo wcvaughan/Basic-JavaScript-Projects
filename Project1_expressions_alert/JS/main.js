57a23857ed05a2016ec132b9b25022e3cfe8080d
wcvaughan <chrisvaughanlds@gmail.com>
2022-07-25 03:57:08 -0800

    The start of JS practice

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,56 @@
+/* We are writing JavaScript! */
+
+window.alert("This is a notice. You have been noticed.");
+//pop-up box with the text displayed and with an "ok" button which closes box when pressed
+
+document.write("Have you noticed?");
+//Displays the text on the page
+
+document.write(" ");
+//Adds a space for formatting
+
+var asdf = "Checking to make sure you have been notified.";
+//assigns a text to the variable "asdf"
+
+document.write(asdf);
+//Displays the text from the variable "asdf" on the page
+
+const tanTamount = " Have you ever seen the rain it's pretty intensely wet";
+//assigns the text to the string "tanTamont"
+
+window.alert(tanTamount);
+//pop-up box with the text of the string "tanTamount" displayed and an "ok" button which closes the box when pressed
+
+const probemblematic = " Can\'t you see? She told me, \"I've seen enough of your shenanigans\"";
+//assigns the text to the string "probemblematic" and uses "\" backslashes as escape markers to allow for parentheses and apostrophes
+
+document.write(probemblematic);
+//Displays the text from the string "probemblematic"
+
+document.write(" Even if you've seen one you" + " haven\'t seen them all.");
+//Concatenates the two strings above and displays the text. Also uses a "\" backslash to allow the apostrophe
+
+var catta = " I've got a lovely bunch of " + "problems that can\'t be solved without more money.";
+//Assigns concatenated strings to variable "catta"
+
+document.write(catta);
+//Displays the text from the variable "catta"
+
+var petsRanked = "Pretty Kitty", firstKitty = "Dante", alrightKitty = "Kila", fatCat = "Pepsi boy", dogCat = "Kitt";
+//Assigns multiple variables
+
+document.write(" ");
+//Adds a space to improve formatting of text
+
+document.write(dogCat);
+//Displays text for variable "dogCat"
+
+var wishPet = (firstKitty + " " + dogCat);
+//Concatenates multiple variables, specifically "firstKitty" and "dogCat", with a space included in the middle, and assigns it to the variable wishPet
+
+document.write(" ");
+//Adds a space to improve formatting of text
+
+document.write(wishPet);
+//Displays text for variable "wishPet"
+
